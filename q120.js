203ca3a218ccc3d09b63948438c73626c06ea08f
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-05-19 00:37:43 +0500

    File of q120

---
+++ q120.js
@@ -0,0 +1,14 @@
+//Use a for...of loop to iterate through an array of your favorite movies and log each one.
+// Defines an array of favorite movies
+var favoriteMoves = [
+    "Mission Impossible",
+    "The Matrix",
+    "FastNFurious",
+    "Rambo",
+];
+// Uses a for...of loop to iterate through favoriteMovies
+for (var _i = 0, favoriteMoves_1 = favoriteMoves; _i < favoriteMoves_1.length; _i++) {
+    var movie = favoriteMoves_1[_i];
+    console.log(movie); // Logs each movie name
+}
+// Each movie in the array is logged to the console, one at a time.
---
+++ q120.ts
@@ -0,0 +1,15 @@
+//Use a for...of loop to iterate through an array of your favorite movies and log each one.
+
+// Defines an array of favorite movies
+const favoriteMoves: string[] = [
+        "Mission Impossible",
+        "The Matrix",
+        "FastNFurious",
+        "Rambo",   
+];
+                                                     // Uses a for...of loop to iterate through favoriteMovies
+for (const movie of favoriteMoves) {
+    console.log(movie);                               // Logs each movie name
+}
+
+  // Each movie in the array is logged to the console, one at a time.
