4e47d26e4c202708fc5a49f7e50039d7b90f8f9e
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-05-19 00:37:15 +0500

    File of q119

---
+++ q119.ts
@@ -0,0 +1,12 @@
+//Create a while loop that logs "Hello, World!" 5 times.
+
+// Initializes a counter
+
+let count:number = 0;
+while(count < 5){                                     // This while loop runs until count is 5
+    console.log("Hello, World!");                        // Logs "Hello, World!"
+    count++;                                                  // Increments count by 1
+}
+
+
+// Repeats logging "Hello, World!" 5 times, once per loop iteration.
